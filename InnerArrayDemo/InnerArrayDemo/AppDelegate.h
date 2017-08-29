86b0aa1ace9203ee3f6bda3224b3cd1d92e565ea
imac <547188371@qq.com>
2017-08-29 19:05:01 +0800

    initializeify

---
+++ InnerArrayDemo/InnerArrayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InnerArrayDemo
+//
+//  Created by imac on 2017/8/29.
+//  Copyright © 2017年 com.InnerArray.GodL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InnerArrayDemo/InnerArrayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InnerArrayDemo
+//
+//  Created by imac on 2017/8/29.
+//  Copyright © 2017年 com.InnerArray.GodL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
